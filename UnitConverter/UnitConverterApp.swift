5c06501b9d582ca2b398a110d9c0d4d1db88fd53
Tugran Yayla <tugrandev@gmail.com>
2023-03-21 14:05:55 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Tuğran on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
